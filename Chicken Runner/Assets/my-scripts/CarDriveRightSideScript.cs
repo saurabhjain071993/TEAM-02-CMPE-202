56cfe8e09fc9db5adbbd3fbc4fb53e8db0dc2fa5
jainbhargav <jainbhargav01@gmail.com>
2016-04-20 19:33:48 -0700

    new script added

---
+++ Chicken Runner/Assets/my-scripts/CarDriveRightSideScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarDriveRightSideScript : MonoBehaviour {
+
+	private GameObject startingPoint;
+	private GameObject endingPoint;
+
+	public float speed = 23.0f;
+
+	// Use this for initialization
+	void Start () {
+		startingPoint = GameObject.Find ("starting-point");
+		endingPoint = GameObject.Find ("ending-point");
+	
+	}
+	
+	// Update will be crated later 
+	
+}
