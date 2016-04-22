a2f05b42e75e71293c4e8b99490108414a3e00c3
bhavin7181 <bhavin.agrawal@sjsu.edu>
2016-04-21 19:53:27 -0700

    #17 Impl module 1 Car Drive Left side

---
+++ Chicken Runner/Assets/my-scripts/CarDriveLeftSideScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarDriveLeftSideScript : MonoBehaviour {
+
+	private GameObject startingPoint;
+	private GameObject endingPoint;
+	
+	public float speed = 23.0f;
+	
+	// Use this for initialization
+	void Start () {
+		startingPoint = GameObject.Find ("starting-point");
+		endingPoint = GameObject.Find ("ending-point");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				
+	}
+}
