e814456e4ae63e5e6b940f0084eff1952592a778
jainbhargav <jainbhargav01@gmail.com>
2016-04-30 16:36:59 -0700

    closes #37

---
+++ Chicken Runner/Assets/my-scripts/ILocation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public interface ILocation
+{
+    float getLocation();
+}
---
+++ Chicken Runner/Assets/my-scripts/LeftLocationDecorator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class LeftLocationDecorator : LocationDecorator
+{
+    private float addedLocation;
+
+    public LeftLocationDecorator(ILocation s)
+        : base(s)
+    {
+
+    }
+
+    public override float getLocation()
+    {
+        addedLocation = base.getLocation();
+        return updatedPosition(addedLocation);
+    }
+
+    public float updatedPosition(float x)
+    {
+        return x - (30.0f * Time.deltaTime);
+    }
+
+}
