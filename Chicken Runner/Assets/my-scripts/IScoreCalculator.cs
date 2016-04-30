fa6f5c4ec14efe54337df11f7c1b146802e7afa4
Ankil0007 <ankil.shah@sjsu.edu>
2016-04-30 01:02:30 -0700

    Created Concrete Component Class and Component Interface
    Decorator Pattern:
    Component : IScoreCalculator
    Concrete Component  : ScoreCalculator

---
+++ Chicken Runner/Assets/my-scripts/IScoreCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public interface IScoreCalculator
+{
+    int getScore();
+}
---
+++ Chicken Runner/Assets/my-scripts/ScoreCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ScoreCalculator : IScoreCalculator
+{
+
+    int currScore;
+    public ScoreCalculator(int currScore)
+    {
+
+        this.currScore = currScore;
+    }
+    public int getScore()
+    {
+        Debug.Log("In getScore");
+        return currScore;
+    }
+    public void invokeAction()
+    {
+
+    }
+}
