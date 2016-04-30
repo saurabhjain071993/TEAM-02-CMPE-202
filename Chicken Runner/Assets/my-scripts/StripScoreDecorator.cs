6e51e38139b48eeac5a6bc90a5f72edc51e4b9bb
Ankil0007 <ankil.shah@sjsu.edu>
2016-04-30 01:06:24 -0700

    Concrete Decorator for strip
    Implemented decorator class for score if obstacle is strip

---
+++ Chicken Runner/Assets/my-scripts/StripScoreDecorator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class StripScoreDecorator : ScoreDecorator
+{
+    private int addedPoints;
+
+    public StripScoreDecorator(IScoreCalculator s)
+        : base(s)
+    {
+
+    }
+
+    public override int getScore()
+    {
+        addedPoints = base.getScore();
+        return addedScore(addedPoints);
+    }
+
+    public int addedScore(int points)
+    {
+        return points + 1;
+    }
+
+}
