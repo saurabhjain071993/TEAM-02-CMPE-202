2a05f94f125227259c1441dc3abf53db24f7b373
bhavin7181 <bhavin.agrawal@sjsu.edu>
2016-04-30 12:52:54 -0700

    # 35 Observer Design pattern
    implementation - 2 Observers added.

---
+++ Chicken Runner/Assets/my-scripts/ObjectAction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ObjectAction : Observer
+{
+    public ObjectAction(TagSubject tagSubject)
+        : base(tagSubject)
+    {
+
+    }
+    public override void invokeAction()
+    {
+        Debug.Log("objct action invoke 420");
+        if (string.Compare(tagSubject.getTag(), "coin", true) == 0)
+        {
+            // global::System.Windows.Forms.MessageBox.Show("Test");
+            Debug.Log("invokeAction coin destroy");
+            Collider other = tagSubject.getOther();
+            tagSubject.getPlayer().destroyObject(other);
+        }
+        else if (string.Equals(tagSubject.getTag(), "strip"))
+        {
+            //do nothing
+        }
+        else if (string.Equals(tagSubject.getTag(), "Enemy"))
+        {
+            tagSubject.getPlayer().killPlayer();
+        }
+        else if (string.Equals(tagSubject.getTag(), "obstacle"))
+        {
+            //do nothing
+        }
+    }
+
+}
---
+++ Chicken Runner/Assets/my-scripts/ScoreObserver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ScoreObserver : Observer
+{
+    public ScoreObserver(TagSubject tagSubject)
+        : base(tagSubject)
+    {
+
+    }
+    public override void invokeAction()
+    {
+        if (string.Equals(tagSubject.getTag(), "coin"))
+        {
+            IScoreCalculator scoreCalculator1 = new CoinScoreDecorator(new ScoreCalculator(tagSubject.getPlayer().score));
+
+            tagSubject.getPlayer().score = scoreCalculator1.getScore();
+            Debug.Log("After" + tagSubject.getPlayer().score);
+            //Increase the Score by one point and add sound
+            //Also destroy the coin
+            Debug.Log("Collision with Coin");
+            //score += 2;
+            tagSubject.getPlayer().scoreText.text = "score: " + tagSubject.getPlayer().score.ToString();
+        }
+        else if (string.Equals(tagSubject.getTag(), "strip"))
+        {
+            IScoreCalculator scoreCalculator1 = new StripScoreDecorator(new ScoreCalculator(tagSubject.getPlayer().score));
+            tagSubject.getPlayer().score = scoreCalculator1.getScore();
+            //score += 1;
+            tagSubject.getPlayer().scoreText.text = "score: " + tagSubject.getPlayer().score.ToString();
+        }
+        else if (string.Equals(tagSubject.getTag(), "Enemy"))
+        {
+            //do nothing
+        }
+        else if (string.Equals(tagSubject.getTag(), "obstacle"))
+        {
+            //do nothing
+        }
+    }
+}
+
