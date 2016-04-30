70776b1760755069fc569a824b06c02134c6e7e8
Ankil0007 <ankil.shah@sjsu.edu>
2016-04-30 01:04:48 -0700

    ConcreteDecorator Class
    CoinScoreDecorator:
    This class has implementation for adding score if obstacle is coin

---
+++ Chicken Runner/Assets/my-scripts/CoinScoreDecorator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CoinScoreDecorator : ScoreDecorator
+{
+    private int addedPoints;
+
+    public CoinScoreDecorator(IScoreCalculator s)
+        : base(s)
+    {
+    }
+
+    public override int getScore()
+    {
+        addedPoints = base.getScore();
+        return addedScore(addedPoints);
+    }
+
+    public int addedScore(int points)
+    {
+        return points + 2;
+    }
+
+}
