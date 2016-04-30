f85a5e3f4ed6ccddc2390d49718374ea6ad071fb
Ankil0007 <ankil.shah@sjsu.edu>
2016-04-30 01:03:39 -0700

    Decorator Class

---
+++ Chicken Runner/Assets/my-scripts/ScoreDecorator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ScoreDecorator : IScoreCalculator
+{
+    private IScoreCalculator scoreCalculator;
+
+    public ScoreDecorator(IScoreCalculator s)
+    {
+
+        this.scoreCalculator = s;
+    }
+    public virtual int getScore()
+    {
+        return scoreCalculator.getScore();
+    }
+}
