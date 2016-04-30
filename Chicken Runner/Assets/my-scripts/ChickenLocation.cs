457c11c529bf86d0dd783fbdd8ad470c761342f2
jainbhargav <jainbhargav01@gmail.com>
2016-04-30 03:35:54 -0700

    Decorator pattern implementation for chicken monvement

---
+++ Chicken Runner/Assets/my-scripts/ChickenLocation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ChickenLocation : ILocation
+{
+
+    float currPosition;
+    public ChickenLocation(float currPosition)
+    {
+
+        this.currPosition = currPosition;
+    }
+    public float getLocation()
+    {
+        Debug.Log("In getScore");
+        return currPosition;
+    }
+}
---
+++ Chicken Runner/Assets/my-scripts/LocationDecorator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class LocationDecorator : ILocation
+{
+    private ILocation location;
+
+    public LocationDecorator(ILocation l)
+    {
+
+        this.location = l;
+    }
+    public virtual float getLocation()
+    {
+        return location.getLocation();
+    }
+}
